e55fe7b9c9d6108e834180be4da0c5f30cd598b6
Quentin Joseph <qej2@icloud.com>
2017-04-13 15:48:28 -0400

    made some changes and additions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var tempFah = 0;
+var tempFah = 70;
 var celcius = (tempFah-32)/1.8;
+var bck2Fah = (celcius*1.8+32);
 
-
-console.log( " The temprature in Celcius is " + celcius + " degrees.")
+console.log( "The temprature in Celcius is " + celcius + " degrees. This program takes the temperature in Fahrenheit, subtracts 32, and then takes that number and devides it by 1.8. Good Times.")
 if (celcius >=32){
 	console.log("whoa... Too hot:/")
 } else if (celcius >=15){
@@ -10,3 +10,4 @@ if (celcius >=32){
 } else {
 	console.log("meh.. :(")
 }
+console.log("This converts Celcius back to Fahrenheit. Which should be "+bck2Fah)
